93afd323fc50ffc0248f16b4f99f930fd5a84a03
rbg92 <rbg92@cornell.edu>
2017-04-21 12:01:29 -0400

    Testing fonts

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>EAST COAST DRONES</title>
-		<link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,700i" rel="stylesheet">
+		<title>East Coast Drones</title>
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700i" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
 		<link href="https://fonts.googleapis.com/css?family=Clicker+Script" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="styles/styles.css">
