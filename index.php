9dd9abee3b84e4e3acb1fcf25181125f421f4f96
rbg92 <rbg92@cornell.edu>
2017-04-21 11:59:20 -0400

    Making title obnoxiously capital letters

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>East Coast Drones</title>
+		<title>EAST COAST DRONES</title>
 		<link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,700i" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
 		<link href="https://fonts.googleapis.com/css?family=Clicker+Script" rel="stylesheet">
