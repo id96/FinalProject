f04230111b4c2ccda78974d1502321fc806d60b5
Isabelle De Brabanter <isabelledebrabanter@dhcp-hol-3866.redrover.cornell.edu>
2017-04-23 19:39:10 -0400

    price page finished

--- price.php
+++ price.php
@@ -17,76 +17,6 @@
 	// $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die ("Unable to connect to MySQL");
 ?>
 
-<p>Pricing by Area</p>
-<p>*If you purchase a Total Package (Aerial Pictures, DSLR Photography, and Video Walk Thru) for any sized Area, receive a $50 discount!*</p>
-
-<table>
-	<td>
-		<h1>0-1999 Sq. Ft:</h1>
-			<p>Aerial Pictures</p>
-				<ul>
-					<li>$75</li>
-				</ul>
-			<p>DSLR Photography</p>
-				<ul>
-					<li>$100</li>
-				</ul>
-			<p>Video Walk Thru (Aerial Video NOT included)</p>
-				<ul>
-					<li>$200</li>
-				</ul>
-			<p>TOTAL = $375, <span id='discount'>$325</span> with discount!</p>
-	</td>
-	<td>
-		<h1>2000-2999 Sq. Ft:</h1>
-			<p>Aerial Pictures</p>
-				<ul>
-					<li>$100</li>
-				</ul>
-			<p>DSLR Photography</p>
-				<ul>
-					<li>$125</li>
-				</ul>
-			<p>Video Walk Thru (Aerial Video NOT included)</p>
-				<ul>
-					<li>$225</li>
-				</ul>
-			<p>TOTAL = $450, <span id='discount'>$400</span> with discount!</p>
-	</td>
-	<td>
-		<h1>3000-4999 Sq. Ft:</h1>
-			<p>Aerial Pictures</p>
-				<ul>
-					<li>$125</li>
-				</ul>
-			<p>DSLR Photography</p>
-				<ul>
-					<li>$150</li>
-				</ul>
-			<p>Video Walk Thru (Aerial Video NOT included)</p>
-				<ul>
-					<li>$250</li>
-				</ul>
-			<p>TOTAL = $525, <span id='discount'>$475</span> with discount!</p>
-	</td>
-	<td>
-		<h1>>5000 Sq. Ft:</h1>
-			<p>Aerial Pictures</p>
-				<ul>
-					<li>$150</li>
-				</ul>
-			<p>DSLR Photography</p>
-				<ul>
-					<li>$175</li>
-				</ul>
-			<p>Video Walk Thru (Aerial Video NOT included)</p>
-				<ul>
-					<li>$275</li>
-				</ul>
-			<p>TOTAL = $600, <span id='discount'>$550</span> with discount!</p>
-	</td>
-</table>
-
 <h1>PRICE GENERATOR!</h1>
 
 <form action="price.php" method="post">
@@ -109,17 +39,16 @@
 	<input class='button' type='checkbox' name='Video'>
 	<label>Video Walk Thru</label>
 	<br>
-	<input class="submit_button" type="submit" name="submit_album" value="Click to submit">
+	<input class="submit_button" type="submit" name='submit' value="Click to submit">
 </form>
 
 <?php
 
-if(isset($_POST['submit'])){
+if(isset($_POST['submit'])) {
 	$total = 0;
-	echo '$total';
 	if(isset($_POST['area'])) {
 		$area = $_POST['area']; 
-		if ($area == '0-1999') {
+		if ($area === '0-1999') {
 			if(isset($_POST['Aerial'])){
 				$total = $total + 75;
 			}
@@ -133,12 +62,122 @@ if(isset($_POST['submit'])){
 				$total = $total - 50;
 			}
 		}
+		if ($area === '2000-2999') {
+			if(isset($_POST['Aerial'])){
+				$total = $total + 100;
+			}
+			if(isset($_POST['DSLR'])){
+				$total = $total + 125;
+			}
+			if(isset($_POST['Video'])){
+				$total = $total + 225;
+			}
+			if(isset($_POST['Aerial'])&&isset($_POST['DSLR'])&&isset($_POST['Video'])){
+				$total = $total - 50;
+			}
+		}
+		if ($area === '3000-4999') {
+			if(isset($_POST['Aerial'])){
+				$total = $total + 125;
+			}
+			if(isset($_POST['DSLR'])){
+				$total = $total + 150;
+			}
+			if(isset($_POST['Video'])){
+				$total = $total + 250;
+			}
+			if(isset($_POST['Aerial'])&&isset($_POST['DSLR'])&&isset($_POST['Video'])){
+				$total = $total - 50;
+			}
+		}
+		if ($area === '>5000') {
+			if(isset($_POST['Aerial'])){
+				$total = $total + 150;
+			}
+			if(isset($_POST['DSLR'])){
+				$total = $total + 175;
+			}
+			if(isset($_POST['Video'])){
+				$total = $total + 275;
+			}
+			if(isset($_POST['Aerial'])&&isset($_POST['DSLR'])&&isset($_POST['Video'])){
+				$total = $total - 50;
+			}
+		}
 	}
-	echo "Total = $total";
+	echo "<p>Total = $$total</p>";
 }
 ?>
 
+<h1>Pricing by Area</h1>
+<p>*If you purchase a Total Package (Aerial Pictures, DSLR Photography, and Video Walk Thru) for any sized Area, receive a $50 discount!*</p>
 
+<table>
+	<td>
+		<h1>0-1999 Sq. Ft:</h1>
+			<p>Aerial Pictures</p>
+				<ul>
+					<li>$75</li>
+				</ul>
+			<p>DSLR Photography</p>
+				<ul>
+					<li>$100</li>
+				</ul>
+			<p>Video Walk Thru (Aerial Video NOT included)</p>
+				<ul>
+					<li>$200</li>
+				</ul>
+			<p>TOTAL = $375, <span class='discount'>$325</span> with discount!</p>
+	</td>
+	<td>
+		<h1>2000-2999 Sq. Ft:</h1>
+			<p>Aerial Pictures</p>
+				<ul>
+					<li>$100</li>
+				</ul>
+			<p>DSLR Photography</p>
+				<ul>
+					<li>$125</li>
+				</ul>
+			<p>Video Walk Thru (Aerial Video NOT included)</p>
+				<ul>
+					<li>$225</li>
+				</ul>
+			<p>TOTAL = $450, <span class='discount'>$400</span> with discount!</p>
+	</td>
+	<td>
+		<h1>3000-4999 Sq. Ft:</h1>
+			<p>Aerial Pictures</p>
+				<ul>
+					<li>$125</li>
+				</ul>
+			<p>DSLR Photography</p>
+				<ul>
+					<li>$150</li>
+				</ul>
+			<p>Video Walk Thru (Aerial Video NOT included)</p>
+				<ul>
+					<li>$250</li>
+				</ul>
+			<p>TOTAL = $525, <span class='discount'>$475</span> with discount!</p>
+	</td>
+	<td>
+		<h1>>5000 Sq. Ft:</h1>
+			<p>Aerial Pictures</p>
+				<ul>
+					<li>$150</li>
+				</ul>
+			<p>DSLR Photography</p>
+				<ul>
+					<li>$175</li>
+				</ul>
+			<p>Video Walk Thru (Aerial Video NOT included)</p>
+				<ul>
+					<li>$275</li>
+				</ul>
+			<p>TOTAL = $600, <span class='discount'>$550</span> with discount!</p>
+	</td>
+</table>
 
 <?php 
 	include 'footer.php';
