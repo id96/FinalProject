7d898e41a21ae0908f8c9a153e5fae714db9402b
Isabelle De Brabanter <isabelledebrabanter@dhcp-hol-3866.redrover.cornell.edu>
2017-04-23 21:12:58 -0400

    portfolio

---
+++ portfolio.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>East Coast Drones</title>
+		<link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,700i" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat">
+		<link href="https://fonts.googleapis.com/css?family=Clicker+Script" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="styles/styles.css">
+	</head>
+<body>
+
+<?php 
+	include 'header.php';
+	// include 'config.php';
+	// $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die ("Unable to connect to MySQL");
+?>
+
+<p>Portfolio</p>
+
+<?php 
+	include 'footer.php';
+?>
+
+</body>
+</html>
