3ba08dd545ce2e216a8a9f94320f447566d465c6
rbg92 <rbg92@cornell.edu>
2017-04-25 16:15:25 -0400

    bootstrap

--- index.php
+++ index.php
@@ -10,18 +10,24 @@
 		<link rel="stylesheet" type="text/css" href="styles/styles.css"<?php echo time(); ?>>
 	</head>
 <body>
+    
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 
-<?php 
-	include 'header.php';
-	// include 'config.php';
-	// $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die ("Unable to connect to MySQL");
-?>
+        <?php 
+            include 'header.php';
+            // include 'config.php';
+            // $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die ("Unable to connect to MySQL");
+        ?>
 
-<p>Home</p>
+                <p>Home</p>
 
-<?php 
-	include 'footer.php';
-?>
-
+        <?php 
+            include 'footer.php';
+        ?>
+                </div>
+            </div>
+    </div>
 </body>
 </html>
