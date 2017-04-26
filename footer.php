826ec94e81d6ba8e22911fdf3e7943a268ed1751
Isabelle De Brabanter <isabelledebrabanter@dhcp-gs-2900.redrover.cornell.edu>
2017-04-25 22:35:37 -0400

    social icons

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <div id="footer">
 	<div>
     	<?php echo date("m/d/Y");?>
-    	<p>Social Media Links Here</p>
+    	<a href='https://www.facebook.com/'><img class='icon' src = 'media/facebook.png' alt = 'Facebook Icon'></a>
+    	<a href='https://www.instagram.com/?hl=en'><img id='insta' src = 'media/insta.png' alt = 'Instagram Icon'></a>
+    	<a href='https://twitter.com/'><img class='icon' src = 'media/twitter.png' alt = 'Twitter Icon'></a>
   	</div>
 </div>
