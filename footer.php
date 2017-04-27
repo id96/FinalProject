f6f547a25066e7e4ee8cbf09f5d381dae711a204
Isabelle De Brabanter <isabelledebrabanter@dhcp-hol-5268.redrover.cornell.edu>
2017-04-26 22:16:46 -0400

    social media icon update

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
 <div id="footer">
 	<div>
-    	<?php echo date("m/d/Y");?>
     	<a href='https://www.facebook.com/'><img class='icon' src = 'media/facebook.png' alt = 'Facebook Icon'></a>
-    	<a href='https://www.instagram.com/?hl=en'><img id='insta' src = 'media/insta.png' alt = 'Instagram Icon'></a>
+    	<a href='https://www.instagram.com/?hl=en'><img class='icon' src = 'media/insta.png' alt = 'Instagram Icon'></a>
     	<a href='https://twitter.com/'><img class='icon' src = 'media/twitter.png' alt = 'Twitter Icon'></a>
+    	<a href='https://google.com/'><img class='icon' src = 'media/google.png' alt = 'Google Icon'></a>
+    	<a href='https://youtube.com/'><img class='icon' src = 'media/youtube.png' alt = 'YouTube Icon'></a>
+    	<a href='https://linkedin.com/'><img class='icon' src = 'media/linkedin.png' alt = 'LinkedIn Icon'></a>
   	</div>
 </div>
