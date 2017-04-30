8d927c50aa6f956a77e3178c48a882a251295c55
rbg92 <rbg92@cornell.edu>
2017-04-30 19:50:51 -0400

    header

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
 <div id="header">
 	<ul>
 		<a href="index.php">HOME</a>
@@ -8,3 +12,4 @@
 		<a href="contact.php">CONTACT</a>
 	</ul>
 </div>
+            </div></div></div>
