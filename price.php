a6bcef26961d137c25f530be2241f0c67457c4f6
Isabelle De Brabanter <isabelledebrabanter@dhcp-gs-72.redrover.cornell.edu>
2017-04-30 20:59:32 -0400

    CSS update to services and price

--- price.php
+++ price.php
@@ -15,9 +15,10 @@
 	// include 'config.php';
 	// $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die ("Unable to connect to MySQL");
 ?>
-<div class='price_body'>
-	<h1>Pricing by Area</h1>
+<div class='price_head'>
+	<h1>Pricing Packages</h1>
 	<p>*If you purchase a Total Package (Aerial Pictures, DSLR Photography, and Video Walk Thru) for any sized Area, receive a $50 discount!*</p>
+</div>
 
 	<!-- SUMMARY TABLE -->
 	<div class="container">
@@ -71,29 +72,31 @@
 
 
 	<div class='price_form'>
-		<h1>PRICE GENERATOR!</h1>
-		<form action="price.php" method="post">
-			<label>Property Area</label>
-				<select class="button" name="area" required>
-					<option value= "0-1999">0-1999 Sq. Ft</option>
-					<option value="2000-2999">2000-2999 Sq. Ft</option>
-					<option value="3000-4999">3000-4999 Sq. Ft</option>
-					<option value=">5000">>5000 Sq. Ft</option>
-				</select>
-			<br>
-			<label>What Services Would You Like?</label>
-			<br>
-			<input class='button' type='checkbox' name='Aerial'>
-			<label>Aerial Pictures</label>
-			<br>
-			<input class='button' type='checkbox' name='DSLR'>
-			<label>DSLR Photography</label>
-			<br>
-			<input class='button' type='checkbox' name='Video'>
-			<label>Video Walk Thru</label>
-			<br>
-			<input class="submit_button" type="submit" name='submit' value="Click to submit">
-		</form>
+		<div class='price_generator'>
+			<h1>PRICE GENERATOR!</h1>
+			<form action="price.php" method="post">
+				<label>Property Area</label>
+					<select class="button" name="area" required>
+						<option value= "0-1999">0-1999 Sq. Ft</option>
+						<option value="2000-2999">2000-2999 Sq. Ft</option>
+						<option value="3000-4999">3000-4999 Sq. Ft</option>
+						<option value=">5000">>5000 Sq. Ft</option>
+					</select>
+				<br>
+				<label>What Services Would You Like?</label>
+				<br>
+				<input class='button' type='checkbox' name='Aerial'>
+				<label>Aerial Pictures</label>
+				<br>
+				<input class='button' type='checkbox' name='DSLR'>
+				<label>DSLR Photography</label>
+				<br>
+				<input class='button' type='checkbox' name='Video'>
+				<label>Video Walk Thru</label>
+				<br>
+				<input class="submit_button" type="submit" name='submit' value="Click to submit">
+			</form>
+		</div>
 	</div>
 
 	<!-- END OF PRICE GENERATOR -->
