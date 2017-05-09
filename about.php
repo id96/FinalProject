9fe26b69da78f4de5631c7b38214044211f4b89d
Isabelle De Brabanter <isabelledebrabanter@Isabelles-MBP.deltadeltadelta.local>
2017-05-08 23:59:52 -0400

    changed footer and price styling

--- about.php
+++ about.php
@@ -5,8 +5,8 @@
 	<meta charset="UTF-8">
     <title>East Coast Drones</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"> 
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"> 
+    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <script src="https://use.fontawesome.com/252db8b05d.js"></script>
