24ebc08f9dae5b1581dc19607123a537b90b3bda
Isabelle De Brabanter <isabelledebrabanter@Isabelles-MBP.deltadeltadelta.local>
2017-05-09 21:04:21 -0400

    about page

--- about.php
+++ about.php
@@ -22,39 +22,38 @@
 	// $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die ("Unable to connect to MySQL");
 ?>
 
-<h1 class="nav-down">About</h1>
-<p class="nav-down">Run by two young professionals who aim to provide our clients with the best work possible through photography and cinematography</p>
+<div class='about_content'>
+	<h1>About</h1>
+	<p>Run by two young professionals who aim to provide our clients with the best work possible through photography and cinematography</p>
 
-
-
-<div class="leftcol_bren">
-	<div class="image">
-		<img class="bio" src="media/brendan.jpg" alt="Brendan Headshot">
+	<div class="leftcol_bren">
+		<div class="image">
+			<img class="bio" src="media/brendan.png" alt="Brendan Headshot">
+		</div>
+		<h1>Brendan Viselli</h1>
+		<p>I am currently a junior at Boston College studying Economics with a minor in History. I am highly motivated and personable. With my creativity I can insure for a product that is beyond that of my competitors. </p>
+		<p>In my free-time I am very active, always enjoying the outdoors. I love ripping around the lakes and oceans on jet skis and boats.</p>
+		<a href="tel:7818355410">(781)-835-5410</a> <br>
+		<a href="mailto:visellib@bc.edu?Subject=Hello%20again" target="_top">visellib@bc.edu</a>
 	</div>
-	<h1>Brendan Viselli</h1>
-	<p>I am currently a junior at Boston College studying Economics with a minor in History. I am highly motivated and personable. With my creativity I can insure for a product that is beyond that of my competitors. </p>
-	<p>In my free-time I am very active, always enjoying the outdoors. I love ripping around the lakes and oceans on jet skis and boats.</p>
-	<a href="tel:7818355410">(781)-835-5410</a> <br>
-	<a href="mailto:visellib@bc.edu?Subject=Hello%20again" target="_top">visellib@bc.edu</a>
-</div>
 
-<div class="rightcol_jon">
-	<div class='image'>
-		<img class="bio" src="media/jon.png" alt="Jon Headshot">
+	<div class="rightcol_jon">
+		<div class='image'>
+			<img class="bio" src="media/jon.png" alt="Jon Headshot">
+		</div>
+		<h1>Jonathan Parece</h1>
+		<p>With this business I hope I can provide clients with the best work at a very competitive price. Incorporating both aerial and walk through video into one video is rarely done and is very effective when done correctly.</p>
+		<p>I have been flying drones for the past 5 years and with that experience I am able to continuously provide a service that displays my creativity. Whether it be for recreational or business use, I am able develop my skills during every flight.</p>
+		<a href="tel:5084239989">(508)-423-9989</a> <br>
+		<a href="mailto:jjparece@loyola.edu?Subject=Hello%20again" target="_top">jjparece@loyola.edu</a>
 	</div>
-	<h1>Jonathan Parece</h1>
-	<p>With this business I hope I can provide clients with the best work at a very competitive price. Incorporating both aerial and walk through video into one video is rarely done and is very effective when done correctly.</p>
-	<p>I have been flying drones for the past 5 years and with that experience I am able to continuously provide a service that displays my creativity. Whether it be for recreational or business use, I am able develop my skills during every flight.</p>
-	<a href="tel:5084239989">(508)-423-9989</a> <br>
-	<a href="mailto:jjparece@loyola.edu?Subject=Hello%20again" target="_top">jjparece@loyola.edu</a>
-</div>
-
-<?php 
-	include 'footer.php';
-    
-?>
-
+</div> <!-- end of content div -->
     
 <!-- echo "<p id='bottom'>Federal Aviation Administration Part 107 Compliant</p>"; -->
+
+<?php 
+	include 'footer.php';  
+?>
+
 </body>
 </html>
