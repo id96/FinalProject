26bef2b0e60ec72b39b91799d0a5a352b07b7e42
cc987@cornell.edu <chelseachan@Chelseas-MacBook-Pro.local>
2017-05-13 13:07:15 -0400

    portfolio page

--- portfolio.php
+++ portfolio.php
@@ -39,73 +39,61 @@
 	<h1>Portfolio</h1>
 </div>
 
-<div> <!-- id="library" -->
-	<?php 
-	// display aerial photos
-	echo "<div class='container'>";
-		echo '<div class="row table">';
-			echo '<div class="plan col-lg-12 col-md-10 col-sm-8">';
-				print("<div class='aerial'>");
-					print('<p>AERIAL</p>');
-					while ($row = $aerial->fetch_assoc()){
-						$media_id = $row['mediaID'];
-						$property_id = $row['propertyID'];
-						$title = $row['title'];
-						$description = $row['description'];
-						$file = $row['file_path'];
-						$type = $row['type_of_photography'];
-						$href = "photo.php?media_id=$media_id"; 
-						
-						// constructs unique url for each photo
-						print("<div class='media col-xs-12 col-sm-12 col-md-3 col-lg-3'>");
-							print("<a href='$href' title='$href'><img src='$file' alt='$title'></a>"); 
-						print("</div>"); //end of media div
-					}
-				print("</div>"); //end of aerial div
-			echo '</div'; // end of plan div
-
-			echo '<div class="plan col-lg-4 col-md-4 col-sm-8">';
-				// display DSLR photos
-				print("<div class='dslr'>");
-					print('<p>DSLR</p>');
-					while ($row = $dslr->fetch_assoc()){
-						$media_id = $row['mediaID'];
-						$property_id = $row['propertyID'];
-						$title = $row['title'];
-						$description = $row['description'];
-						$file = $row['file_path'];
-						$type = $row['type_of_photography'];
-						$href = "photo.php?media_id=$media_id"; 
-						
-						// constructs unique url for each photo
-						print("<div class='media col-xs-12 col-sm-12 col-md-3 col-lg-3'>");
-							print("<a href='$href' title='$href'><img src='$file' alt='$title'></a>"); 
-						print("</div>"); // end of media div
-					}
-				print("</div>"); //end of dslr div
-			echo '</div'; //end of plan div 
-	
-
-			echo '<div class="plan col-lg-4 col-md-4 col-sm-8">';
-				// display video walk thru
-				print("<div class='video'>");
-					print('<p>VIDEO WALK-THRU</p>');
-						print("<div class='media'>");
-							echo '<iframe width="300" height="245" src="https://www.youtube.com/embed/BGz-CwtJP6Q" frameborder="0" allowfullscreen></iframe>';
-						print("</div>"); // end of media div
-				//print("</div>"); // end of video div
-			echo '</div'; // end of plan div 
-
-		echo '</div'; //end of row table div
-	echo '</div'; //end of container div
-?>
-</div>
-
-
-
 <?php 
-	include 'footer.php';
+// display aerial photos
+echo "<div class='container'>";
+	echo '<div class="row table">';
+		echo '<div class="aerial col-lg-12 col-md-10 col-sm-8">';
+				print('<h1>AERIAL</h1>');
+				while ($row = $aerial->fetch_assoc()){
+					$media_id = $row['mediaID'];
+					$property_id = $row['propertyID'];
+					$title = $row['title'];
+					$description = $row['description'];
+					$file = $row['file_path'];
+					$type = $row['type_of_photography'];
+					$href = "photo.php?media_id=$media_id"; 
+			
+					// constructs unique url for each photo
+					print("<div class='media col-xs-12 col-sm-12 col-md-2 col-lg-2'>");
+						print("<a href='$href' title='$href'><img src='$file' alt='$title'></a>"); 
+					print("</div>"); //end of media div
+				}
+		echo '</div>'; // end of aerial div
+
+		echo '<div class="dslr col-lg-12 col-md-10 col-sm-8">';
+			// display DSLR photos
+				print('<h1>DSLR</h1>');
+				while ($row = $dslr->fetch_assoc()){
+					$media_id = $row['mediaID'];
+					$property_id = $row['propertyID'];
+					$title = $row['title'];
+					$description = $row['description'];
+					$file = $row['file_path'];
+					$type = $row['type_of_photography'];
+					$href = "photo.php?media_id=$media_id"; 
+					
+					// constructs unique url for each photo
+					print("<div class='media col-xs-12 col-sm-12 col-md-3 col-lg-3'>");
+						print("<a href='$href' title='$href'><img src='$file' alt='$title'></a>"); 
+					print("</div>"); // end of media div
+				}
+		echo '</div>'; //end of dslr div 
+
+		echo '<div class="video col-lg-12 col-md-10 col-sm-8">';
+			// display video walk thru
+				print('<h1>VIDEO WALK-THRU</h1>');
+				print("<div class='media'>");
+					echo '<iframe width="300" height="245" src="https://www.youtube.com/embed/BGz-CwtJP6Q" frameborder="0" allowfullscreen></iframe>';
+				print("</div>"); // end of media div
+		echo '</div>'; // end of video div 
+
+	echo '</div>'; //end of row table div
+
+echo '</div>'; //end of container div
 ?>
+
+<?php include 'footer.php';?>
 
 </body>
 </html>
