89f0302720b7e3166234bea4f93934b4bdc5b1dd
Isabelle De Brabanter <isabelledebrabanter@dhcp-gs-4774.redrover.cornell.edu>
2017-05-16 11:40:08 -0400

    fixed bootstrap files

--- logout.php
+++ logout.php
@@ -15,14 +15,13 @@
 <head>
 	<meta charset="UTF-8">
     <title>East Coast Drones</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <script src="https://code.jquery.com/jquery-latest.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
     <script src="https://use.fontawesome.com/252db8b05d.js"></script>
 	<link rel="stylesheet" type="text/css" href="styles/styles.css">
     <script type="text/javascript" src="js/main.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
 </head>
 <body>
 	<?php include 'header.php'; ?>
--- price.php
+++ price.php
@@ -5,12 +5,11 @@
 		<meta charset="UTF-8">
 	    <title>East Coast Drones</title>
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"> 
 	    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
-	    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 	    <script src="https://use.fontawesome.com/252db8b05d.js"></script>
 		<link rel="stylesheet" type="text/css" href="styles/styles.css"<?php echo time(); ?>>
+		<link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
 	    <script type="text/javascript" src="js/main.js"></script>
 	</head>
 <body>
